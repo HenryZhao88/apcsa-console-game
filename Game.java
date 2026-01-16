1055b95bfe45e92370c7f7bd0456bf9e0aaa0cad
Henry Zhao <henryozhao@gmail.com>
2026-01-16 06:24:17 -0500

    titan implemented

--- Game.java
+++ Game.java
@@ -1,1 +1,32 @@
+public class Game {
+    public static void main(String[] args) {
+        Adventurer player1 = new Titan("Jynxi", 4090);
+        Adventurer enemy = new Titan("Leo", 4090); // Using Titan as enemy for testing
 
+        System.out.println("Beginning Battle!");
+        System.out.println(player1.getName() + " HP: " + player1.getHP() + " Special: " + player1.getSpecial());
+        System.out.println(enemy.getName() + " HP: " + enemy.getHP());
+        System.out.println();
+
+        // toss
+        System.out.println(player1.attack(enemy));
+        System.out.println(enemy.getName() + " HP: " + enemy.getHP());
+        System.out.println(player1.getName() + " Special: " + player1.getSpecial()); // Should increase
+        System.out.println();
+
+        // special (punch)
+        System.out.println(player1.specialAttack(enemy));
+        System.out.println(enemy.getName() + " HP: " + enemy.getHP());
+        System.out.println(player1.getName() + " Special: " + player1.getSpecial()); // Should decrease
+        System.out.println();
+
+        // support self
+        System.out.println(player1.support());
+        System.out.println(player1.getName() + " HP: " + player1.getHP());
+        System.out.println();
+
+        // support others
+        System.out.println(player1.support(enemy));
+        System.out.println(enemy.getName() + " HP: " + enemy.getHP());
+    }
+}
---
+++ Titan.java
@@ -0,0 +1,83 @@
+public class Titan extends Adventurer {
+    private int energy;
+    private int energyMax;
+
+    public Titan(String name, int hp) {
+        super(name, hp);
+        this.energyMax = 100;
+        this.energy = 50;
+    }
+
+    public Titan(String name) {
+        this(name, 4090);
+    }
+
+    public Titan() {
+        this("Bob");
+    }
+
+    @Override
+    public String getSpecialName() {
+        return "Energy";
+    }
+
+    @Override
+    public int getSpecial() {
+        return energy;
+    }
+
+    @Override
+    public void setSpecial(int n) {
+        this.energy = n;
+    }
+
+    @Override
+    public int getSpecialMax() {
+        return energyMax;
+    }
+
+    @Override
+    public String attack(Adventurer other) {
+        int damage = 135;
+        other.applyDamage(damage);
+        restoreSpecial(5); // Regain energy on basic hit
+        return this.getName() + " uses Toss on " + other.getName() + " dealing " + damage + " damage!";
+    }
+
+    @Override
+    public String specialAttack(Adventurer other) {
+        if (getSpecial() >= 25) {
+            setSpecial(getSpecial() - 25);
+            int damage = 253;
+            other.applyDamage(damage);
+            return this.getName() + " uses Punch on " + other.getName() + " dealing " + damage + " damage!";
+        } else {
+            return "Not enough energy to Punch! Instead " + attack(other);
+        }
+    }
+
+    @Override
+    public String support(Adventurer other) {
+        int heal = 50;
+        int max = other.getmaxHP();
+        if(other.getHP() + heal > max){
+            other.setHP(max);
+        }else{
+            other.setHP(other.getHP() + heal);
+        }
+        return this.getName() + " encourages " + other.getName() + ", healing them for " + heal + " HP!";
+    }
+
+    @Override
+    public String support() {
+        int heal = 100;
+        int max = getmaxHP();
+        if(getHP() + heal > max){
+            setHP(max);
+        }else{
+            setHP(getHP() + heal);
+        }
+        restoreSpecial(10);
+        return this.getName() + " heals self for " + heal + " HP and recovers Energy.";
+    }
+}
