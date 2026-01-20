95462b08466efe57ef24b191d5158b804d619a09
ckuo70 <carterk6@nycstudents.net>
2026-01-19 21:34:14 -0500

    Implement player card selection in Game
    Updated the Game class to allow player card selection via user input.

--- Game.java
+++ Game.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
-        Card player1 = new Titan("Jynxi", 4090);
-        Card enemy = new Titan("Leo", 4090); // Using Titan as enemy for testing
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello! Welcome to Battle Crown! Each player will choose one of three cards: Paladin, Titan, and LP (Lil' P.L.E.K.K.A)! Player 1, choose your card!");
+        String c1 = input.nextLine();
+        while (!(c1.equals("LP") || c1.equals("Titan") || c1.equals("Paladin"))){
+            System.out.println("Please select Paladin, Titan, or LP.");
+            c1 = input.nextLine();
+        }
+        Card player1;
+        if (c1.equals("LP")){player1 = new LP();}
+        else if (c1.equals("Titan")){player1 = new Titan();}
+        else {player1 = new Paladin();}
+
+        System.out.println("Alright! Player 1 has selected " + c1 + ". Player 2, choose your card!");
+        String c2 = input.nextLine();
+        while (!(c2.equals("LP") || c2.equals("Titan") || c2.equals("Paladin"))){
+            System.out.println("Please select Paladin, Titan, or LP.");
+            c2 = input.nextLine();
+        }
+        Card player2;
+        if (c2.equals("LP")){player2 = new LP();}
+        else if (c2.equals("Titan")){player2 = new Titan();}
+        else {player2 = new Paladin();}
+
+        System.out.println("Let the battle begin!");
+        int turn = 1;
+        while (player1.getHP() > 0 && player2.getHP() > 0){
+            if (turn == 1){
+                System.out.println("It is player 1's turn. Choose from the following actions: attack, special attack HENRY I STOPPED HERE")
+            }
+        }
 
         System.out.println("Beginning Battle!");
         System.out.println(player1.getName() + " HP: " + player1.getHP() + " Special: " + player1.getSpecial());
